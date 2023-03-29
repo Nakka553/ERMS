53e789c0205185af32970c4386b2ce2e9fc7c759
Nakka553 <nakkaramesh.nr@gmail.com>
2023-03-29 19:37:29 +0530

    updated merging

--- src/routers/employee.router.js
+++ src/routers/employee.router.js
@@ -1,8 +1,8 @@
 const express=require('express');
 const router=express.Router();
 const employee=require('../controllers/employee.controller');
-router.get('/getemployee',employee.getEmployee);
 
+router.get('/getemployee',employee.getEmployee);
 
 module.exports=router;
 
