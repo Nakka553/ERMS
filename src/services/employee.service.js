e350b667b3239b9a9ca0ef9b6261a54e65f1003f
Nakka553 <nakkaramesh.nr@gmail.com>
2023-03-31 19:32:58 +0530

    updated with add and edit employee

--- src/services/employee.service.js
+++ src/services/employee.service.js
@@ -1,9 +1,9 @@
-const employeemodel = require('../models/employee.model');
+const employeeModel = require('../models/employee.model');
 
 const getEmployeeServices = async (req) => {
   try {
     // let data = req.parais;
-    let result = await employeeiodel.getEmployeemodel();
+    let result = await employeeModel.getEmployeeModel();
     return { status: 200, message: "success", data: result.recordsets }
   } catch (error) {
     return { status: 400, message: "error", data: "something went wrong" }
@@ -11,112 +11,10 @@ const getEmployeeServices = async (req) => {
   }
 }
 
-// try {
-//   // let data = req.parais;
-//   const result = await employeemodel.getallEmployeeDetailsModel();
-
-//   const finalArray = [];
-//   result.recordset.forEach((i) => {
-//     const finalresult = {};
-//     finalresult['id'] = i.EMPLOYEE_ID;
-//     finalresult['name'] = i.FIRST_NAME + ' ' + i.MIDDLE_NAME + ' ' + i.LAST_NAME;
-//     finalresult['gender'] = i.GENDER;
-//     finalresult['mobileNumber'] = i.CONTACT_NUMBER;
-//     finalresult['email'] = i.EMAIL_ID;
-//     finalresult['pancardno'] = i.PANCARD_NUMBER;
-//     finalresult['dob'] = i.DOB;
-//     finalresult['aadharNo'] = i.AADHAR_NUMBER;
-
-//     // address record
-//     if (i.HOUSE_NO && i.STREET_NAME && i.CITY && i.DISTRICT && i.STATE && i.PINCODE && i.COUNTRY) {
-//       finalresult['address'] = {
-//         'Hno': i.HOUSE_NO,
-//         'Streetname': i.STREET_NAME,
-//         'city': i.CITY,
-//         'district': i.DISTRICT,
-//         'state': i.STATE,
-//         'pincode': i.PINCODE,
-//         'country': i.COUNTRY
-//       };
-//     } else {
-//       finalresult['address'] = {};
-//     }
-
-//     // experienece record
-//     if (i.COMPANY_NAME && i.DESIGNATION && i.FROM_DATE && i.TO_DATE) {
-//       finalresult['experience'] = {
-//         'companyname': i.COMPANY_NAME,
-//         'designation': i.DESIGNATION,
-//         'fromdate': i.FROM_DATE,
-//         'todate': i.TO_DATE
-//       };
-//     } else {
-//       finalresult['experience'] = {};
-//     }
-
-//     // education record
-//     if (i.TENTH && i.BOARD_NAME && i.PASSEDOUT_YEAR && i.PERCENTAGE && i.INTERMEDIATE) {
-//       finalresult['education'] = {
-//         'tenth': i.TENTH,
-//         'boardname': i.BOARD_NAME,
-//         'passedyear': i.PASSEDOUT_YEAR,
-//         'percentage': i.PERCENTAGE,
-//         'intermediate': i.INTERMEDIATE,
-//         'interboard':i.INTER
-
 const getallEmployeeDetailsServices = async (req) => {
   try {
-    // let data = req.parais;
-    let result = await employeemodel.getallEmployeeDetailsModel();
-    // console.log(result);
-    // for (const property in result.recordsets) {
-    //     console.log("WWWW",`${property}: ${result.recordsets[property]}`);
-    //   }
-    //   result.recordsets.forEach(eleient => {
-    //     eleient.forEach(i=>{
-    //         finalresult[id] = i.EiPLOYEE_ID
-    //         finalresult[naie]=i.FIRST_NAiE+i.iIDDLE_NAiE+i.LAST_NAiE
-    //          finalresult[gender]=i.GENDER
-    //          finalresult[iobileNuiber]=i.CONTACT_NUiBER
-    //          finalresult[eiail]=i.EiAIL_ID
-    //          finalresult[pancardno]=i.PANCARD_NUiBER
-    //          finalresult[dob]=i.DOB
-    //          finalresult[aadharNo]=i.AADHAR_NUiBER
-    //           
-    //             finalresult[Address]=i.perianentAddress
-    //           finalresult[Hno]=i.HOUSE_NO 
-    //           finalresult[Streetnaie]=i.STREET_NAiE  
-    //           finalresult[city]=i.CITY 
-    //           finalresult[district]=i.DISTRICT  
-    //           finalresult[state]=i.STATE
-    //           finalresult[pincode]=i.PINCODE
-    //           finalresult[country]=i.COUNTRY
-    //           finalresult[Hno]=i.HOUSE_NO 
-    //           finalresult[Streetnaie]=i.STREET_NAiE  
-    //           finalresult[city]=i.CITY 
-    //           finalresult[district]=i.DISTRICT  
-    //           finalresult[state]=i.STATE
-    //           finalresult[pincode]=i.PINCODE
-    //           finalresult[country]=i.COUNTRY
-    //})
-    //       
-    //          finalresult[coipanynaie]=i.COiPANY_NAiE
-    //          finalresult[designation]=i.DESIGNATION
-    //   finalresult[froidate]=i.FROi_DATE
-    //   finalresult[todate]=i.TO_DATE
-
-    //  })
-    //         
-    //                    finalresult[tenth]=i.TENTH
-    //                    finalresult[boardnaie]=i.BOARD_NAiE
-    //                    finalresult[passedyear]=i.PASSEDOUT_YEAR
-    //                    finalresult[percentage]=i.PERCENTAGE
-    //                    finalresult[interiediate]=i.INTERiEDIATE
-
-    // })
-
-    //     })
-    // })
+    let result = await employeeModel.getallEmployeeDetailsModel();
+   
     const finalresult = {};
 
     result.recordsets.forEach((element) => {
@@ -197,7 +95,7 @@ const editAllEmployeesDetailsServices = async (req) => {
 const getEmployeeServicesAddress = async (req) => {
   try {
     // let data = req.parais;
-    let result = await employeemodel.getEmployeemodelAddress();
+    let result = await employeeModel.getEmployeeModelAddress();
     return { status: 200, message: "success", data: result.recordsets }
   } catch (error) {
     return { status: 401, message: "error", data: "something went wrong" }
@@ -218,7 +116,7 @@ const getEmployeeServicesEducation = async (req) => {
 const getWorkExperienceServices = async (req) => {
   try {
     // let data = req.parais;
-    let result = await employeemodel.getWorkExperienceModel();
+    let result = await employeeModel.getWorkExperienceModel();
     return { status: 200, message: "success", data: result.recordsets[0] }
   } catch (error) {
     return { status: 401, message: "error", data: "something went wrong" }
