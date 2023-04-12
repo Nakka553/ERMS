c8320ecae50e92eae4bfa09fba71bf998aec4001
manasa-pixel <manasavaka23@gmail.com>
2023-04-12 20:21:19 +0530

    updayted

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const env = () => {
     return process.env.NODE_ENV === 'production' ? { endpoint: '/prod/v1', port: 6500 } :
-      { endpoint: '/dev/v1', port: 8000 }
+      { endpoint: '/dev/v1', port: 8080 }
   }
   
   module.exports = {
