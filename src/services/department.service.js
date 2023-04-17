26df8c917ff08776c8ee9b87ce1adf2fa266b9f2
Nakka553 <nakkaramesh.nr@gmail.com>
2023-04-17 16:38:24 +0530

    latest

--- src/services/department.service.js
+++ src/services/department.service.js
@@ -3,15 +3,12 @@ const departmentModel=require('../models/department.model');
 
 const getDepartmentServices = async () =>{
     try {
-        if(result?.recordset?.recordsets[0]){
+        
         // let data = req.params;
         let result=await departmentModel.getDepartmentModel();
         return {status:200,message:"success",data:result.recordset}
     } 
-    else{
-        return{status:401,message:"fail",data:"Their is no data"}
-      }
-    }
+    
     catch (error) {
     return{status:401,message:error.message,data:"something went wrong"}      
     }
@@ -19,15 +16,11 @@ const getDepartmentServices = async () =>{
 
 const addDepartmentServices = async (req) =>{
     try {
-        if(result?.recordset?.recordsets[0]){
         let data = req.body;
         let result=await departmentModel.addDepartmentModel(data);
         return {status:200,message:"success",data:[]}
     } 
-    else{
-        return{status:401,message:"fail",data:"Their is no data"}
-      }
-    }
+    
     catch (error) {
     return{status:401,message:"error",data:"something went wrong"}   
     }
@@ -35,15 +28,12 @@ const addDepartmentServices = async (req) =>{
 
 const editDepartmentServices = async (req) =>{
     try {
-        if(result?.recordset?.recordsets[0]){
+        
         let data = req.body;
         let result=await departmentModel.editDepartmentModel(data);
         return {status:200,message:"success",data:[]}
     } 
-    else{
-        return{status:401,message:"fail",data:"Their is no data"}
-      }
-    }
+    
     catch (error) {
     return{status:401,message:"error",data:"something went wrong"}   
     }
@@ -52,15 +42,12 @@ const editDepartmentServices = async (req) =>{
 
 const deleteDepartmentServices = async (req) =>{
     try {
-        if(result?.recordset?.recordsets[0]){
+        
         let data = req.params;
         let result=await departmentModel.deleteDepartmentModel(data);
         return {status:200,message:"success",data:[]}
     } 
-    else{
-        return{status:401,message:"fail",data:"Their is no data"}
-      }
-    }
+    
     catch (error) {
     return{status:401,message:"error",data:"something went wrong"}   
     }
