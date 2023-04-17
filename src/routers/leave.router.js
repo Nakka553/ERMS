7b6ef6491335d63ec15b8fad90052ec4d234c610
Nakka553 <nakkaramesh.nr@gmail.com>
2023-04-17 10:43:27 +0530

    latest code

--- src/routers/leave.router.js
+++ src/routers/leave.router.js
@@ -3,10 +3,6 @@ const router = express.Router() ;
 const leaveController= require("../controllers/leave.controller")
 
 const {verifyToken}=require('../../express/auth')
-
-
-
-
 router.get('/getLeave',verifyToken,leaveController.getLeaveController);
 router.post('/addLeave',verifyToken,leaveController.addLeaveController);
 router.put('/editLeave',verifyToken,leaveController.editLeaveController);
